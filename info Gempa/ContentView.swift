de89f3ed13cc0dbf6158e89af0a01d1a6a7dd941
Cong Fandi <cong.fandi@abersoft.se>
2021-07-09 08:03:06 +0700

    Initial Commit

---
+++ info Gempa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  info Gempa
+//
+//  Created by Cong Fandi on 09/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ info Gempa/info_GempaApp.swift
@@ -0,0 +1,17 @@
+//
+//  info_GempaApp.swift
+//  info Gempa
+//
+//  Created by Cong Fandi on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct info_GempaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
